79d9997aec853645f8fe3bde78a4d82ac3f4d796
Nitheesh K V <166894014+Nitheesh141@users.noreply.github.com>
2025-01-08 18:45:11 +0530

    Time: 0 ms (100%), Space: 10.5 MB (63.14%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool valid(string temp){
+        if(temp.size()>3 || temp.size()==0) return false;
+        if(temp.size()>1 && temp[0]=='0')   return false;
+        if(temp.size() && stoi(temp)>255) return false; 
+        return true;
+    }
+
+    void solve(vector<string>& ans, string output, int ind, string s, int dots){
+        if(dots == 3){ 
+            if(valid(s.substr(ind)))
+                ans.push_back(output+s.substr(ind));
+            return;
+        }
+        int sz = s.size();
+        for(int i=ind;i<min(ind+3, sz);i++){
+            if(valid(s.substr(ind, i-ind+1))){
+                output.push_back(s[i]);
+                output.push_back('.');
+                solve(ans, output, i+1, s, dots+1);
+                output.pop_back();
+            }
+        }
+
+    }
+
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> ans;
+        string res;
+        solve(ans, res, 0, s, 0);
+        return ans;
+    }
+};
